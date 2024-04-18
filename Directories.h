e11dee6f5c114962d37d7d053778ffd56ade1e1d
adrux444 <rachel.dixon10@icloud.com>
2024-04-18 17:24:00 +0100

    made getSubDirectories and getFiles functions

--- Directories.h
+++ Directories.h
@@ -1,20 +1,34 @@
 #pragma once
 #include <string>
+#include <vector>
 #include "systemItem.h"
 
-class Directory :
-    public systemItem
-{
-public:
-    Directory(const std::string& name, bool isDir, const std::string& size, const std::string& date) : name(name), isDir(isDir), size(size), date(date) {}
-    std::string getName() const override { return name; }
-    bool isDirectory() const override { return isDir; }
-    std::string getSize() const override { return size; }
-    std::string getDate() const override { return date; }
-private:
-    std::string name;
-    bool isDir;
-    std::string size;
-    std::string date;
-};
+using namespace std;
 
+class Directory : public systemItem {
+public:
+    Directory(const string& name, bool isDir, const string& size, const string& date);
+    string getName() const override { return name; }
+    bool isDirectory() const override { return isDir; }
+    string getSize() const override { return size; }
+    string getDate() const override { return date; }
+    void addSubDirectory(Directory* directory) {
+        subDirectories.push_back(directory);
+    }
+    void addFile(systemItem* file) {
+        files.push_back(file);
+    }
+    const vector<Directory*>& getSubDirectories() const {
+        return subDirectories;
+    }
+    const vector<systemItem*>& getFiles() const {
+        return files;
+    }
+private:
+    string name;
+    bool isDir;
+    string size;
+    string date;
+    vector<Directory*> subDirectories;
+    vector<systemItem*> files;
+};
--- systemItem.cpp
+++ systemItem.cpp
@@ -1,17 +1,2 @@
 #include "systemItem.h"
 
-std::string systemItem::getName() const {
-    return "";
-}
-
-std::string systemItem::getSize() const {
-    return "";
-}
-
-std::string systemItem::getDate() const {
-    return "";
-}
-
-bool systemItem::isDirectory() const {
-    return false;
-}
